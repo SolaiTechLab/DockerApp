fcf02e308eddaef3436349e7b1f3e95dafc41fe1
Solairaj.Mariappan <Solairaj.Mariappan@ihsmarkit.com>
2022-12-28 22:05:10 +0530

    Base code checkin

---
+++ CoreApplication/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        for(int i = 0; i < 100; i++)
+        {
+            Console.WriteLine("\nThe item ID is {0}", i);
+        }
+        Console.WriteLine("Thank You !!");
+    }
+}
